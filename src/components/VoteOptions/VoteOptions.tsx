905c132d5addcc1a5e77633e7ccb9510b1bb04bf
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-04-25 21:22:10 +0300

    feat: complete Step 8 and add Notification component

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,25 @@
+import css from './VoteOptions.module.css';
+
+
+interface VoteOptionsProps {
+  onVote: (type: 'good' | 'neutral' | 'bad') => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote('good')}>Good</button>
+      <button className={css.button} onClick={() => onVote('neutral')}>Neutral</button>
+      <button className={css.button} onClick={() => onVote('bad')}>Bad</button>
+      
+      {/* Кнопка Reset показується тільки якщо canReset === true */}
+      {canReset && (
+        <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
